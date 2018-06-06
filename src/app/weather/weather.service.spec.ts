9bbed312d886bec4305ec63252c88f7ff40a8da8
usbpendrive <ferry.fernando@gmail.com>
2018-06-06 12:40:02 +0700

    Spec files

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { WeatherService } from './weather.service'
+import {WeatherServiceFake} from './weather.service.fake';
 
 describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [WeatherService],
+      imports: [HttpClientTestingModule],
+      providers: [{WeatherService, useClass: WeatherServiceFake}],
     })
   })
 
